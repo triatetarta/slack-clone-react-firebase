637ec6fc042442b99bd604055c7826049000e111
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-18 15:36:06 +0000

    finished header styling with styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,90 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <h1>test</h1>
+      <HeaderLeft>
+        <HeaderAvatar src='' />
+        <AccessTimeIcon />
+      </HeaderLeft>
+
+      <HeaderSearch>
+        <SearchIcon />
+        <input type='text' placeholder='Search Alania' />
+      </HeaderSearch>
+
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   );
 };
 
 const HeaderContainer = styled.div`
-  color: violet;
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.75rem 0;
+  background-color: var(--slack-color);
+  color: #fff;
+`;
 
-  h1 {
-    font-size: 5rem;
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 1.25rem;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 1.75rem;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 3.1rem;
+  color: gray;
+  border: 1px solid gray;
+
+  > input {
+    border: none;
+    background-color: transparent;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: #fff;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 1.75rem;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
   }
 `;
 
