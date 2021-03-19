83951669b746eb9aa2655bc7bc56f3348ea53f3f
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-19 09:07:02 +0000

    sidebar main done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import Chat from './components/Chat';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
     <div>
       <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Header />
-          </Route>
-        </Switch>
+        <Header />
+        <AppBody>
+          <Sidebar />
+          <Switch>
+            <Route exact path='/'>
+              <Chat />
+            </Route>
+          </Switch>
+        </AppBody>
       </Router>
     </div>
   );
 };
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <SidebarHeader>
+        <SidebarInfo>
+          <h2>Three Quarters</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Dimitrios Chatziioannou
+          </h3>
+        </SidebarInfo>
+        <CreateIcon />
+      </SidebarHeader>
+    </SidebarContainer>
+  );
+};
+
+const SidebarContainer = styled.div`
+  color: #fff;
+  background-color: var(--slack-color);
+  flex: 0.3;
+  border-top: 1px solid #49274b;
+  max-width: 260px;
+  margin-top: 3.75rem;
+`;
+
+const SidebarHeader = styled.div`
+  display: flex;
+  border-bottom: 1px solid #49274b;
+  padding: 0.8rem;
+  align-items: center;
+
+  > .MuiSvgIcon-root {
+    padding: 0.5rem;
+    color: #49274b;
+    font-size: 1.125rem;
+    background-color: #fff;
+    border-radius: 100%;
+    cursor: pointer;
+  }
+`;
+
+const SidebarInfo = styled.div`
+  flex: 1;
+
+  > h2 {
+    font-size: 1rem;
+    font-weight: 900;
+    margin-top: 0.3rem;
+  }
+
+  > h3 {
+    display: flex;
+    font-size: 0.8rem;
+    font-weight: 400;
+    align-items: center;
+
+    > .MuiSvgIcon-root {
+      font-size: 0.7rem;
+      margin-top: 2px;
+      margin-right: 2px;
+      color: green;
+    }
+  }
+`;
+
+export default Sidebar;
