a03fa5be9449707957524138888d2176b2454438
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-19 09:51:56 +0000

    basic firebase setup and init

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyB1yD9wygupOEzq9Vm_HxY73lI-jpaknUA',
   authDomain: 'slack-clone-74ac4.firebaseapp.com',
@@ -6,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: '954306564163',
   appId: '1:954306564163:web:9a003cbf9fc76374a044f6',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
